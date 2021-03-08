447924f87030f6f931c489d02a4102636375a40e
Enrique Kessler Martínez <qkessler@gmail.com>
2021-03-08 18:56:29 +0100

    Adding changes to the routing, trying to understand React components

--- 100days-of-gatsby_1/gatsby-config.js
+++ 100days-of-gatsby_1/gatsby-config.js
@@ -1,21 +1,4 @@
 module.exports = {
     plugins: [
-        `gatsby-transformer-remark`,
-        {
-            resolve: `gatsby-transformer-yaml`,
-            options: {
-                // Conditionally set the typeName so that we both use a lowercased and capitalized type name
-                typeName: ({ node }) => {
-                    const name = node.sourceInstanceName
-                    if (name === `cities`) {
-                        return `City`
-                    }
-                    if (name === `parks`) {
-                        return `park`
-                    }
-                    return name
-                },
-            },
-        },
     ]
 }
