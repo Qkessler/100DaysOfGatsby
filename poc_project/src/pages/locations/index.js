4305054b4c825e69d9e49e62b17768eeaea57bf6
Enrique Kessler Martínez <qkessler@gmail.com>
2021-03-18 17:29:34 +0100

    11. Adding changes to emacs configuration, working with lsp-mode

--- poc_project/src/pages/locations/index.js
+++ poc_project/src/pages/locations/index.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
-import wrapRootElement from "../../components/root-wrapper.js" 
 
 // styles
 const pageStyles = {
@@ -24,68 +23,31 @@ const listStyles = {
   marginBottom: 96,
   paddingLeft: 0,
 }
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
 
 const LocationPage = ({ data }) => {
   return (
     <wrapRootElement>
-    <main style={pageStyles}>
-      <title>Locations page</title>
-      <h1 style={headingStyles}>
-        Hi traveler!
+      <main style={pageStyles}>
+        <title>Locations page</title>
+        <h1 style={headingStyles}>
+          Hi traveler!
         <br />
-        <span style={headingAccentStyles}>— you are at the Locations page! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🌆🌆🌆
+          <span style={headingAccentStyles}>— you are at the Locations page! </span>
+          <span role="img" aria-label="Party popper emojis">
+            🌆🌆🌆
         </span>
-      </h1>
-      <p style={paragraphStyles}>
-        You will be able to select from the Locations list to access the different places
-        we currently work at.
+        </h1>
+        <p style={paragraphStyles}>
+          You will be able to select from the Locations list to access the different places
+          we currently work at.
       </p>
-      <ul style={listStyles}>
-        {data.allContentfulLocation.nodes.map((node) => (
-          <li><Link to={node.gatsbyPath}>{node.name}</Link></li>
-        ))}
-      </ul>
-    </main>
-      </wrapRootElement>
+        <ul style={listStyles}>
+          {data.allContentfulLocation.nodes.map((node) => (
+            <li><Link to={node.gatsbyPath}>{node.name}</Link></li>
+          ))}
+        </ul>
+      </main>
+    </wrapRootElement>
   )
 }
 
