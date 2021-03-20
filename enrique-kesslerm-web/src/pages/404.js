8913fe70aa3d71c7ed59e63fac9eef6d22e44af5
Enrique Kessler Martínez <qkessler@gmail.com>
2021-03-20 20:45:40 +0100

    12. Changing the personal web starter with orgajs; org files as posts

--- enrique-kesslerm-web/src/pages/404.js
+++ enrique-kesslerm-web/src/pages/404.js
@@ -1,10 +1,10 @@
-import React from "react"
+import React from 'react'
+import { Layout } from 'gatsby-theme-blorg'
 
-import Layout from "../components/layout"
-
-export default () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+export default () => {
+  return (
+    <Layout>
+      <div>Not Found</div>
+    </Layout>
+  )
+}
