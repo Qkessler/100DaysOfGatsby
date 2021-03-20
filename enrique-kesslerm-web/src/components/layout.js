bbf5e0d5a023e92f1522f76d2191ac9e621fb24a
Enrique Kessler Martínez <qkessler@gmail.com>
2021-03-20 12:46:35 +0100

    12. Working changes for adding the NavBar to the app.

--- enrique-kesslerm-web/src/components/layout.js
+++ enrique-kesslerm-web/src/components/layout.js
@@ -1,22 +1,15 @@
 import React from 'react'
-// import { Link } from 'gatsby'
+import NavBar from './navbar'
+import {ChakraProvider} from '@chakra-ui/react'
 
-// import NavBar from './navbar'
-import {GrMenu, GrClose} from 'react-icons/gr'
-import {Box} from '@chakra-ui/react'
 
 export default ({ children }) => {
   return (
-    <div>
-      <Box w="100%">
-        {GrMenu}
-      </Box>
-      <Box w="100%">
-        {GrClose}
-      </Box>
+    <ChakraProvider>
+      <NavBar/>
       <main>
         { children }
       </main>
-    </div>
+    </ChakraProvider>
   )
 }
