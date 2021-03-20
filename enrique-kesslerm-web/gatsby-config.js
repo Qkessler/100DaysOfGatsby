a5dbc505ede181e5f1de81a4de214d4f29dfc298
Enrique Kessler Martínez <qkessler@gmail.com>
2021-03-20 21:01:12 +0100

    12. Adding changes to the gatsby-config.js file, configuring org options

--- enrique-kesslerm-web/gatsby-config.js
+++ enrique-kesslerm-web/gatsby-config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  pathPrefix: `/gatsby-starter-blorg`,
+  // pathPrefix: `/gatsby-starter-blorg`,
   siteMetadata: {
-    siteURL: 'https://orga.js.org', // replace this with your own domain
-    title: `Gatsby With Orga`, // replace it with your own title
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: 'Xiaoxing Hu', // this one is take, choose another name
-    twitter: 'xiaoxinghu', // replace it, or set to 'false' to disable "Tweet this" button
+    siteURL: 'https://enriquekesslerm.com',
+    title: `Enrique Kessler Martínez`,
+    description: `Enrique Kessler Martínez's personal web and blog.`,
+    author: 'Enrique Kessler Martínez',
+    twitter: 'quique_kessler', // replace it, or set to 'false' to disable "Tweet this" button
     social: [ // again, replace thse with your own value, or set to empty array to disable them
-      { name: 'twitter', url: 'https://twitter.com/xiaoxinghu' },
-      { name: 'website', url: 'https://www.huxiaoxing.com' },
-      { name: 'github', url: 'https://github.com/xiaoxinghu' },
+      { name: 'twitter', url: 'https://twitter.com/quique_kessler' },
+      { name: 'website', url: 'https://enriquekesslerm.com' },
+      { name: 'github', url: 'https://github.com/Qkessler' },
     ]
   },
   plugins: [
@@ -19,7 +19,7 @@ module.exports = {
         // contentPath: 'content',
         // filter: () => true,
         // pagination: 5,
-        // columns: 2,
+        columns: 1,
         // indexPath: '/',
         // imageMaxWidth: 1380,
         // categoryIndexPath: category => `/${category}`,
